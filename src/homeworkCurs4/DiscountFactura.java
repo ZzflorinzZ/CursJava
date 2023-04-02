03916514a8299db3db437e57d68acb1326a8b036
Ispas Florian <ispas.florian@gmail.com>
2023-04-02 21:53:05 +0300

    tema curs4 final

--- src/homeworkCurs4/DiscountFactura.java
+++ src/homeworkCurs4/DiscountFactura.java
@@ -1,38 +1,36 @@
 package homeworkCurs4;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class DiscountFactura {
 
 	public static void main(String[] args) {
 		
-/*		
+		
 	//varianta mai complexa
 		
-		double factura;
+		String factura;
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Please enter the invoice value");
-		factura = scan.nextDouble();
+		factura = scan.next();
 		
 		
-		double finalValue = (factura > 100) ? factura - factura * 10/100 : (0 < factura && factura < 100) ?
-					factura - factura * 5/100 : "Please enter a positive value";
-		System.out.println(finalValue);
+		String finalValue = (Double.valueOf(factura) > 100) ? 
+				String.valueOf(Double.valueOf(factura) - Double.valueOf(factura) * 10/100) : 
+				(0 < Double.valueOf(factura) && Double.valueOf(factura) < 100) ? 
+				String.valueOf(Double.valueOf(factura) - Double.valueOf(factura) * 5/100) : 
+				"Please enter a positive value";
+		System.out.printf("%.2f", Double.valueOf(finalValue));
+		System.out.println();
 		
-		/*
-		 * Cum se pot folosi data type-uri diferite intr-o variabila careia ii este alocat un Ternary operator?
-		 * 
-		 * daca definesc String factura si String finalValue ==> operatorii nu functioneaza pt data type String
-		 * 
-		 * daca definesc double factura si String finalValue ==> nu poate face conversia din double in String
-		 * 
-		 * daca definesc double factura si double finalValue si vreau sa printez si un String ==> nu poate face conversia din String in double
-		 * 
-		 */
+		System.out.println("===============================================");
 
 
-
+	//=====================================================================
+		
+		
 	//varianta simpla - nu trateaza valori negative
 		
 		double factura2;
@@ -43,16 +41,36 @@ public class DiscountFactura {
 		
 		
 		double finalValue2 = (factura2 > 100) ? factura2 - factura2 * 10/100 : factura2 - factura2 * 5/100;
-		System.out.println(finalValue2);
-
-		/*
-		 * se poate limita numarul de zecimale folosit de double?
-		 * 
-		 * daca valoarea facturii introduse este cu 2 zecimale (ex. 44.6), face calculul cu pana la 15 zecimale 
-		 * si as vrea sa returneze mereu rezultatul cu maxim 2 zecimale 
-		 * 
-		 */
 		
+		DecimalFormat df = new DecimalFormat("0.00");
+		
+		System.out.println(df.format(finalValue2));
+
+		
+		System.out.println("===============================================");
+		
+		
+	//=====================================================================
+		
+		
+		double number =  10.2134546878678;
+
+		System.out.println("First print :" + number);
+
+		System.out.printf("Second print :" + "%.2f", number);
+
+		System.out.println();
+
+		System.out.println("Third print :" +Math.round(number));
+
+		DecimalFormat df1 = new DecimalFormat("0.0");      
+
+		System.out.println("Fourth print :" + df1.format(number));
+
+		DecimalFormat df2 = new DecimalFormat("0.000");      
+
+		System.out.println("Fifth print :" + df2.format(number));
+	
 		
 	}
 
